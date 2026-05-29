165ecd900f75af4227e67f9bea72fa25a18de20d
Pedro-Naganotour <pedro@naganotour.com.br>
2026-05-29 13:19:06 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { PassageirosPage } from "@/pages/PassageirosPage";
 import { PassageiroPerfilPage } from "@/pages/PassageiroPerfilPage";
 import { StubPage } from "@/components/stub-page";
 import { KanbanPage } from "@/pages/KanbanPage";
-
+import { VagasPage } from "@/pages/VagasPage";
 
 export default function App() {
   return (
@@ -31,7 +31,7 @@ export default function App() {
         <Route path="kanban/documentacao" element={<StubPage title="Kanban Documentação" description="Controle das etapas documentais." />} />
         <Route path="kanban/coe-visto" element={<StubPage title="Kanban COE / Visto" description="Acompanhamento de COE, visto e exigências." />} />
         <Route path="kanban/embarque" element={<StubPage title="Kanban Embarque" description="Reconsulta, passagens, contratos, exames e embarques." />} />
-        <Route path="vagas" element={<StubPage title="Vagas" description="Cadastro e consulta de vagas disponíveis no Japão." />} />
+        <Route path="vagas" element={<VagasPage />} />
         <Route path="empreiteiras" element={<StubPage title="Empreiteiras" description="Cadastro de empreiteiras parceiras." />} />
         <Route path="fabricas" element={<StubPage title="Fábricas" description="Cadastro de fábricas vinculadas às empreiteiras." />} />
         <Route path="despachantes" element={<StubPage title="Despachantes" description="Cadastro de despachantes e custos de visto." />} />
